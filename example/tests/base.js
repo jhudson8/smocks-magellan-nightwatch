189c8fa5ae9c665af96ca7332522e2e88efcbe36
Joe Hudson <joehud@gmail.com>
2015-12-05 09:01:44 -0500

    more docs

--- example/tests/base.js
+++ example/tests/base.js
@@ -7,7 +7,10 @@ SuperBase.selectorToken = function (value) {
 var util = require('util');
 
 /**
- * Base class which provides global setup / teardown
+ * base class used for the test classes (in ./message-tests).  it's only real value
+ * is to expose an "appUrl" function.  a global variable is introduced called "appPort"
+ * which is the port onn localhost that the current nightwatch worker is running
+ * the application on
  */
 var Base = function (steps) {
   SuperBase.call(this, steps);
