d4bb6b000feed30edf28aff654a085dea4f87e96
Joe Hudson <joehud@gmail.com>
2016-01-15 15:17:39 -0500

    refactoring

--- setup-teardown.js
+++ setup-teardown.js
@@ -8,9 +8,9 @@ var fs = require('fs');
 global.suppressMockServer = true;
 
 module.exports = function (options) {
-  options.distPath = path.join(__dirname, '.dist');
-  options.indexFileName = 'index.html';
-  options.indexFilePath = path.join(options.distPath, options.indexFileName);
+  var logFile = options.logFile;
+  var builder = options.build;
+  var distPath = path.join(__dirname, '.dist');
 
   var SetupTeardown = function () {
   };
@@ -20,7 +20,7 @@ module.exports = function (options) {
     initialize: function () {
       var deferred = Q.defer();
 
-      build(options, function (err) {
+      build(function (err) {
         if (err) {
           return deferred.reject(err);
         }
@@ -33,43 +33,50 @@ module.exports = function (options) {
     flush: function () {
       var deferred = Q.defer();
 
-      rmdir(options.distPath, function () {
+      rmdir(distPath, function (err) {
+        if (err) {
+          log(distPath + ' directory not cleaned up: ' + err.message);
+        } else {
+          log(distPath + ' directory cleaned up');
+        }
+
         deferred.resolve();
       })
       return deferred.promise;
     }
   };
 
-  return SetupTeardown;
-}
+  function build (callback) {
+    log('building app: ' + distPath);
 
-function build (options, callback) {
-  var distPath = options.distPath;
-  log('building app: ' + distPath, options);
-
-  mkdirp(distPath, function (err) {
-    if (err) {
-      log(distPath + ' could not be created: ' + err.message, options);
-      console.log('could not create directory: ', distPath);
-      return callback(err);
-    }
-
-    log(distPath + ' directory created', options);
-    options.build(options.distPath, function (err) {
+    mkdirp(distPath, function (err) {
       if (err) {
-        log('application build error: ' + err.message, options);
-        console.log('application build error: ', err);
+        log(distPath + ' could not be created: ' + err.message);
         return callback(err);
       }
-      log('application built... starting tests', options);
-      console.log('application built... starting tests');
-      callback();
-    });
-  });
-}
 
-function log (message, options) {
-  if (options.logFile) {
-    fs.appendFileSync(options.logFile, message + '\n', {encoding: 'utf8'});
+      log(distPath + ' directory created');
+      builder(distPath, function (err) {
+        if (err) {
+          log('application build error: ' + err.message);
+          console.log('application build error: ', err);
+          return callback(err);
+        }
+        log('application built... starting tests');
+        callback();
+      });
+    });
   }
+
+  function log (message) {
+    if (logFile) {
+      try {
+        fs.appendFileSync(logFile, message + '\n', {encoding: 'utf8'});
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
+  }
+
+  return SetupTeardown;
 }
